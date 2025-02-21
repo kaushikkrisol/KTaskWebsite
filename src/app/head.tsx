5648c4b40aa7da9bc387a6fabd1c80f2a027f8db
ADMIN <kaushik@krisolinfosoft.com>
2025-02-21 17:16:20 +0530

    New Changes

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,6 +5,28 @@ export default function Head() {
       Play Next.js - SaaS Starter Kit and Boilerplate for Next.js
       </title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="keywords" content="
+          Prepress task management software,
+          Printing project workflow management,
+          Print production task automation,
+          Prepress job scheduling tools,
+          Digital prepress workflow optimization,
+          Prepress job tracking system,
+          Task management for printing companies,
+          Printing process workflow automation,
+          Print shop task management system,
+          Prepress task tracking solutions,
+          Automated prepress workflow solutions,
+          Prepress job management software,
+          Print production task tracking,
+          Prepress process automation,
+          Digital print workflow software,
+          Prepress task monitoring tools,
+          Online prepress job tracking,
+          Print workflow optimization tools,
+          Printing workflow automation software,
+          Customizable prepress workflow
+        " />
       <meta name="description" content="This SaaS Boilerplate and Starter Kit for Next.js is designed specifically for SaaS startups. It's a free resource complete with all the necessary integrations, pages, and components you require to build and launch a comprehensive SaaS website with robust features." />
       <link rel="icon" href="/images/logoImg.png" />
     </>
